5ee5585d5d04be03cf6ed148bee2fa0904d73e0c
JoseOgam <jotienoochieng@gmail.com>
2020-06-24 07:33:49 -0400

    initial commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,13 +1,34 @@
 var mongoose = require("mongoose");
+var validator= require("validator");
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{useUnifiedTopology:true, useCreateIndex:true});
 
 
 var User = mongoose.model('User', {
 name:{
-    type : String
+    type : String,
+    required: true,
+    trim: true
+},
+email:{
+    type:String,
+    required:true,
+    trim:true,
+    lowercase:true,
+    validate(value){
+        if(!validator.isEmail(value)){
+            throw new Error ('Email is invalid');
+        }
+    }
 },
 age:{
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value){
+        if(value<0){
+            throw new Error ('Age must be a positive number');
+            
+        }
+    }
 }
 });
 
@@ -27,4 +48,27 @@ person.save().then(()=>{
     console.log(person);
 }).catch((err)=>{
     console.log("error!", err);
+});
+
+
+//task model
+
+var Task = mongoose.model("Task",{
+description:{
+    type: String
+},
+completed:{
+    type: Boolean
+}
+})
+
+var work = new Task({
+    description: "coding at lakehub",
+    completed: false
+})
+
+work.save().then(()=>{
+    console.log(work);
+}).catch((err)=>{
+    console.log("Error!", err);
 });
