324168f86a6adf1fa28e4272ff3a16b60950d3e4
JoseOgam <jotienoochieng@gmail.com>
2020-06-30 14:47:36 -0400

    created routes for various models in nodes app

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ var port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
 app.listen(port, () => {
   console.log(`server is up on port ${port}`);
 });
