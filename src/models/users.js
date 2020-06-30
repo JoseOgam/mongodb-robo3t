60af5c5649793fca12e681419777a9f9eb207f64
JoseOgam <jotienoochieng@gmail.com>
2020-06-30 18:37:57 -0400

    working on login method

--- src/models/users.js
+++ src/models/users.js
@@ -1,34 +1,67 @@
 var mongoose = require("mongoose");
-var validator= require("validator");
+var validator = require("validator");
+var bcrypt = require("bcrypt");
+
+var userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 8,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("password cannot contain passwoed");
+      }
+    },
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  var user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("unable to login");
+  }
+  var isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("wrong passwords");
+  }
+  return user;
+};
 
 //create models
-var User = mongoose.model('User', {
-    name:{
-        type : String,
-        required: true,
-        trim: true
-    },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error ('Email is invalid');
-            }
-        }
-    },
-    age:{
-        type: Number,
-        default: 0,
-        validate(value){
-            if(value<0){
-                throw new Error ('Age must be a positive number');
-                
-            }
-        }
-    }
-    });
+var User = mongoose.model("User", userSchema);
 
-    module.exports = User;
+module.exports = User;
--- src/router/users.js
+++ src/router/users.js
@@ -75,4 +75,14 @@ router.delete("/users/:id", async (req, res) => {
   }
 });
 
+//login
+router.post("/users/login", async (req, res) => {
+  try {
+    var user = await User.findByCredentials(req.body.email, req.body.password);
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 module.exports = router;
